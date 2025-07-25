retrixe/octyne

commit 1a26f0abbb23332b127ae23885b2927c4d191134

Fix memory leak when reloading watching users.json

auth/memory.go
@@ -10,14 +10,19 @@ import (
 
 // MemoryAuthenticator is an Authenticator implementation using an array to store tokens.
 type MemoryAuthenticator struct {
-	Users  *xsync.MapOf[string, string]
-	Tokens *xsync.MapOf[string, string]
+	Users       *xsync.MapOf[string, string]
+	userUpdates chan []byte
+	Tokens      *xsync.MapOf[string, string]
 }
 
 // NewMemoryAuthenticator initializes an authenticator using memory for token storage.
 func NewMemoryAuthenticator(usersJsonPath string) *MemoryAuthenticator {
-	users := CreateUserStore(usersJsonPath)
-	return &MemoryAuthenticator{Tokens: xsync.NewMapOf[string, string](), Users: users}
+	users, userUpdates := createUserStore(usersJsonPath)
+	return &MemoryAuthenticator{
+		Users:       users,
+		userUpdates: userUpdates,
+		Tokens:      xsync.NewMapOf[string, string](),
+	}
 }
 
 // GetUser returns info about the user with the given username.
@@ -93,7 +98,8 @@ func (a *MemoryAuthenticator) Logout(token string) (bool, error) {
 	return tokenExisted, nil
 }
 
-// Close closes the authenticator. This is no-op for MemoryAuthenticator.
-func (*MemoryAuthenticator) Close() error {
+// Close closes the authenticator. Once closed, the authenticator should not be used.
+func (a *MemoryAuthenticator) Close() error {
+	close(a.userUpdates)
 	return nil
 }

auth/redis.go
@@ -12,14 +12,15 @@ import (
 
 // RedisAuthenticator is an Authenticator implementation using Redis to store tokens.
 type RedisAuthenticator struct {
-	Users *xsync.MapOf[string, string]
-	Redis *redis.Pool
-	URL   string
+	Users       *xsync.MapOf[string, string]
+	userUpdates chan []byte
+	Redis       *redis.Pool
+	URL         string
 }
 
 // NewRedisAuthenticator initializes an authenticator using Redis for token storage.
 func NewRedisAuthenticator(usersJsonPath string, url string) *RedisAuthenticator {
-	users := CreateUserStore(usersJsonPath)
+	users, userUpdates := createUserStore(usersJsonPath)
 	pool := &redis.Pool{
 		Wait:      true,
 		MaxIdle:   5,
@@ -32,7 +33,7 @@ func NewRedisAuthenticator(usersJsonPath string, url string) *RedisAuthenticator
 			return conn, err
 		},
 	}
-	return &RedisAuthenticator{Redis: pool, URL: url, Users: users}
+	return &RedisAuthenticator{Redis: pool, URL: url, Users: users, userUpdates: userUpdates}
 }
 
 // GetUser returns info about the user with the given username.
@@ -125,5 +126,6 @@ func (a *RedisAuthenticator) Logout(token string) (bool, error) {
 
 // Close closes the authenticator. Once closed, the authenticator should not be used.
 func (a *RedisAuthenticator) Close() error {
+	close(a.userUpdates)
 	return a.Redis.Close()
 }

auth/users.go
@@ -24,7 +24,7 @@ func ValidateUsername(username string) string {
 	return ""
 }
 
-func CreateUserStore(usersJsonPath string) *xsync.MapOf[string, string] {
+func createUserStore(usersJsonPath string) (*xsync.MapOf[string, string], chan []byte) {
 	// Create default users.json file
 	_, err := os.Stat(usersJsonPath)
 	if os.IsNotExist(err) {
@@ -62,10 +62,10 @@ func CreateUserStore(usersJsonPath string) *xsync.MapOf[string, string] {
 	err = json.Unmarshal(initialFile, &usersJson)
 	if err != nil {
 		log.Println("An error occurred while parsing " + usersJsonPath + "! " + err.Error())
-		return users
+		return users, updates
 	}
 	updateUserStoreFromMap(users, usersJson)
-	return users
+	return users, updates
 }
 
 func updateUserStoreFromMap(users *xsync.MapOf[string, string], userMap map[string]string) error {

system/watch.go
@@ -9,7 +9,7 @@ import (
 
 // ReadAndWatchFile reads a file and returns its contents.
 // It also starts a goroutine that watches for changes to the file and sends updates on a channel.
-func ReadAndWatchFile(filePath string) ([]byte, <-chan []byte, error) {
+func ReadAndWatchFile(filePath string) ([]byte, chan []byte, error) {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, nil, err

commit 69843bc8d49ff850546b372e94e721781c090414

Fix user update regression in 1a26f0a

auth/memory.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"context"
 	"errors"
 	"log"
 	"net/http"
@@ -10,18 +11,18 @@ import (
 
 // MemoryAuthenticator is an Authenticator implementation using an array to store tokens.
 type MemoryAuthenticator struct {
-	Users       *xsync.MapOf[string, string]
-	userUpdates chan []byte
-	Tokens      *xsync.MapOf[string, string]
+	Users           *xsync.MapOf[string, string]
+	stopUserUpdates context.CancelFunc
+	Tokens          *xsync.MapOf[string, string]
 }
 
 // NewMemoryAuthenticator initializes an authenticator using memory for token storage.
 func NewMemoryAuthenticator(usersJsonPath string) *MemoryAuthenticator {
-	users, userUpdates := createUserStore(usersJsonPath)
+	users, stopUserUpdates := createUserStore(usersJsonPath)
 	return &MemoryAuthenticator{
-		Users:       users,
-		userUpdates: userUpdates,
-		Tokens:      xsync.NewMapOf[string, string](),
+		Users:           users,
+		stopUserUpdates: stopUserUpdates,
+		Tokens:          xsync.NewMapOf[string, string](),
 	}
 }
 
@@ -100,6 +101,6 @@ func (a *MemoryAuthenticator) Logout(token string) (bool, error) {
 
 // Close closes the authenticator. Once closed, the authenticator should not be used.
 func (a *MemoryAuthenticator) Close() error {
-	close(a.userUpdates)
+	a.stopUserUpdates()
 	return nil
 }

auth/redis.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"context"
 	"errors"
 	"log"
 	"net/http"
@@ -12,15 +13,15 @@ import (
 
 // RedisAuthenticator is an Authenticator implementation using Redis to store tokens.
 type RedisAuthenticator struct {
-	Users       *xsync.MapOf[string, string]
-	userUpdates chan []byte
-	Redis       *redis.Pool
-	URL         string
+	Users           *xsync.MapOf[string, string]
+	stopUserUpdates context.CancelFunc
+	Redis           *redis.Pool
+	URL             string
 }
 
 // NewRedisAuthenticator initializes an authenticator using Redis for token storage.
 func NewRedisAuthenticator(usersJsonPath string, url string) *RedisAuthenticator {
-	users, userUpdates := createUserStore(usersJsonPath)
+	users, stopUserUpdates := createUserStore(usersJsonPath)
 	pool := &redis.Pool{
 		Wait:      true,
 		MaxIdle:   5,
@@ -33,7 +34,7 @@ func NewRedisAuthenticator(usersJsonPath string, url string) *RedisAuthenticator
 			return conn, err
 		},
 	}
-	return &RedisAuthenticator{Redis: pool, URL: url, Users: users, userUpdates: userUpdates}
+	return &RedisAuthenticator{Redis: pool, URL: url, Users: users, stopUserUpdates: stopUserUpdates}
 }
 
 // GetUser returns info about the user with the given username.
@@ -126,6 +127,6 @@ func (a *RedisAuthenticator) Logout(token string) (bool, error) {
 
 // Close closes the authenticator. Once closed, the authenticator should not be used.
 func (a *RedisAuthenticator) Close() error {
-	close(a.userUpdates)
+	a.stopUserUpdates()
 	return a.Redis.Close()
 }

auth/users.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
@@ -24,7 +25,7 @@ func ValidateUsername(username string) string {
 	return ""
 }
 
-func createUserStore(usersJsonPath string) (*xsync.MapOf[string, string], chan []byte) {
+func createUserStore(usersJsonPath string) (*xsync.MapOf[string, string], context.CancelFunc) {
 	// Create default users.json file
 	_, err := os.Stat(usersJsonPath)
 	if os.IsNotExist(err) {
@@ -42,13 +43,17 @@ func createUserStore(usersJsonPath string) (*xsync.MapOf[string, string], chan [
 	}
 
 	users := xsync.NewMapOf[string, string]()
-	initialFile, updates, err := system.ReadAndWatchFile(usersJsonPath)
+	fileUpdates, cancel, err := system.ReadAndWatchFile(usersJsonPath)
 	if err != nil {
+		// panic here, as this is critical for authenticator and we don't want to continue without it
 		log.Panicln("An error occurred while reading " + usersJsonPath + "! " + err.Error())
 	}
 	go (func() {
 		for {
-			newFile := <-updates
+			newFile, ok := <-fileUpdates
+			if !ok {
+				return
+			}
 			var usersJson map[string]string
 			err = json.Unmarshal(newFile, &usersJson)
 			if err != nil {
@@ -58,14 +63,7 @@ func createUserStore(usersJsonPath string) (*xsync.MapOf[string, string], chan [
 			updateUserStoreFromMap(users, usersJson)
 		}
 	})()
-	var usersJson map[string]string
-	err = json.Unmarshal(initialFile, &usersJson)
-	if err != nil {
-		log.Println("An error occurred while parsing " + usersJsonPath + "! " + err.Error())
-		return users, updates
-	}
-	updateUserStoreFromMap(users, usersJson)
-	return users, updates
+	return users, cancel
 }
 
 func updateUserStoreFromMap(users *xsync.MapOf[string, string], userMap map[string]string) error {

system/watch.go
@@ -2,6 +2,7 @@ package system
 
 import (
 	"bytes"
+	"context"
 	"log"
 	"os"
 	"time"
@@ -9,19 +10,20 @@ import (
 
 // ReadAndWatchFile reads a file and returns its contents.
 // It also starts a goroutine that watches for changes to the file and sends updates on a channel.
-func ReadAndWatchFile(filePath string) ([]byte, chan []byte, error) {
+func ReadAndWatchFile(filePath string) (<-chan []byte, context.CancelFunc, error) {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, nil, err
 	}
+	ctx, cancel := context.WithCancel(context.Background())
 	channel := make(chan []byte, 1)
+	channel <- file // Send the initial file content to the channel
 	go (func() {
 		for {
 			select {
-			case _, more := <-channel:
-				if !more {
-					return
-				}
+			case <-ctx.Done():
+				close(channel)
+				return
 			case <-time.After(1 * time.Second):
 			}
 			newFile, err := os.ReadFile(filePath)
@@ -35,5 +37,5 @@ func ReadAndWatchFile(filePath string) ([]byte, chan []byte, error) {
 			}
 		}
 	})()
-	return file, channel, nil
+	return channel, cancel, nil
 }